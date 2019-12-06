8c9c42fa0cfa699f87ab4537255da61afe667306
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2019-12-06 14:38:32 +0900

    Add tokenize test

--- src/main.rs
+++ src/main.rs
@@ -29,3 +29,34 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+#[test]
+fn test_tokenize() {
+    use token::*;
+
+    let input = "\
+# This is comment.
+ADDI	r8,	r0,	1
+ADD	r9,	r0,	r8
+";
+
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut buf = String::new();
+    let mut reader = BufReader::new(input.as_bytes());
+    while reader.read_line(&mut buf).unwrap() > 0 {
+        lexer::tokenize(&buf, &mut tokens);
+        buf.clear();
+    }
+
+    assert_eq!(tokens[0].kind, TokenKind::ADDI);
+    assert_eq!(tokens[1].kind, TokenKind::REG("r8".to_string()));
+    assert_eq!(tokens[2].kind, TokenKind::REG("r0".to_string()));
+    assert_eq!(tokens[3].kind, TokenKind::INT(1));
+    assert_eq!(tokens[4].kind, TokenKind::EOL);
+    assert_eq!(tokens[5].kind, TokenKind::ADD);
+    assert_eq!(tokens[6].kind, TokenKind::REG("r9".to_string()));
+    assert_eq!(tokens[7].kind, TokenKind::REG("r0".to_string()));
+    assert_eq!(tokens[8].kind, TokenKind::REG("r8".to_string()));
+    assert_eq!(tokens[9].kind, TokenKind::EOL);
+
+}
+
