4bf530e071b5babd92110e98a2bc422eabc335c5
kmgy <bhnk1270223@gn.iwasaki.ac.jp>
2021-01-10 17:27:51 +0900

    Added parsing of command line arguments

---
+++ src/cli.rs
@@ -0,0 +1,36 @@
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
+use super::token::Tokens;
+use super::token::memory::Memory;
+use super::lexer::tokenize;
+use super::parser::parse;
+
+pub fn run(files: &[&str]) {
+    let mut tokens: Tokens = Tokens::new();
+    let mut memory = Memory::default();
+
+    // Join files  =>  Everyone global
+    for (filename_idx, file) in files.iter().enumerate() {
+        let mut number_of_lines: u32 = 1;
+        let mut reader = BufReader::new(File::open(file).expect("Failed file open"));
+        tokens.add_file(&file);
+
+        let mut buf = String::new();
+        while 0 < reader.read_line(&mut buf).unwrap() {
+            if let Err(e) = tokenize(number_of_lines, filename_idx, &buf, &mut tokens) {
+                eprintln!("{}:{}: {}", file, number_of_lines, e);
+                std::process::exit(1);
+            }
+            number_of_lines += 1;
+            buf.clear();
+        }
+    }
+
+    // Execute
+    if let Err(e) = parse(&mut tokens, &mut memory) {
+        eprintln!("{}", e);
+        std::process::exit(1);
+    }
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,29 @@
+#[macro_use]
+extern crate clap;
+
+mod cli;
 mod repl;
 mod token;
 mod lexer;
 mod parser;
 
-use std::env;
-use std::io::{BufRead, BufReader};
-
-use token::*;
-use token::memory::Memory;
+use clap::{Arg, App};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("mipsi")
+        .version(crate_version!())
+        .about(crate_description!())
+        .arg(Arg::with_name("file").multiple(true))
+        .get_matches();
 
-    if args.len() == 1 {
-        // REPL
+    // REPL
+    if matches.values_of("file").is_none() {
         repl::run();
-
-    } else {
-        // Join files  =>  Everyone global
-        let mut tokens: Tokens = Tokens::new();
-
-        for (filename_idx, file) in args.iter().skip(1).enumerate() {
-            let mut number_of_lines: u32 = 1;
-            let mut reader = BufReader::new(std::fs::File::open(file).expect("Failed file open"));
-            tokens.add_file(&file);
-
-            let mut buf = String::new();
-            while reader.read_line(&mut buf).unwrap() > 0 {
-                if let Err(e) = lexer::tokenize(number_of_lines, filename_idx, &buf, &mut tokens) {
-                    panic!("tokenize failed: {}", e);
-                }
-                number_of_lines += 1;
-                buf.clear();
-            }
-        }
-
-        let mut memory = Memory::default();
-
-        parser::parse(&mut tokens, &mut memory).unwrap();
-
-        //println!("{:?}", tokens);
-        //parser::display::display_data_per_4byte(&memory.static_data);
-        //parser::display::display_stack(&memory.stack);
-        //parser::display::display_register(&memory.registers);
-        //parser::display::display_fp_register(&memory.registers);
+        return;
     }
+
+    // CLI
+    let files: Vec<&str> = matches.values_of("file").unwrap().collect();
+    cli::run(&files);
 }
 
